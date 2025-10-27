a8c7659ce732a5498b5cd997f36cdcb70450aba8
Marc Gueury <marc.gueury@oracle.com>
2025-10-27 19:06:33 +0100

    v1

--- src/terraform/botsdk_apigw.tf
+++ src/terraform/botsdk_apigw.tf
@@ -15,3 +15,22 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment_botsdk" {
   }
   freeform_tags = local.api_tags
 }  
+
+
+resource "oci_core_security_list" "starter_security_list_botsdk" {
+  compartment_id = local.lz_network_cmp_ocid
+  vcn_id         = oci_core_vcn.starter_vcn.id
+  display_name   = "${var.prefix}-security-list"
+
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = "0.0.0.0/0"
+    stateless = false
+
+    tcp_options {
+      min = 3000
+      max = 3000
+    }
+  }
+  freeform_tags = local.freeform_tags
+}
